cebbae9e9d51c560a48f5e6a643255b987240dd5
Christian James Gingco <gingcochristian@gmail.com>
2025-03-30 15:25:16 +1100

    Add markdown schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,7 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import {media} from 'sanity-plugin-media'
+import {markdownSchema} from 'sanity-plugin-markdown'
 
 export default defineConfig({
   name: 'default',
@@ -11,7 +12,7 @@ export default defineConfig({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: process.env.SANITY_STUDIO_DATASET || "production",
 
-  plugins: [structureTool(), visionTool(), media()],
+  plugins: [structureTool(), visionTool(), media(), markdownSchema()],
 
   schema: {
     types: schemaTypes,
